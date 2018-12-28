e6e239d1d5de3d709dfe99daf587c02ccdfbd912
이동현 <dlehdanakf@konkuk.ac.kr>
2018-12-28 15:17:35 +0900

    Index 메인 페이지 버튼 문구 수정

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -38,7 +38,7 @@
 			$basicCard->title = "건국대학교 학우에게만 제공되는 다양한 무료 이벤트!";
 			$basicCard->setThumbnail((new Thumbnail($temporary_thumbnail, $temporary_alt)));
 			$basicCard->addButton((new Button("최신 문화초대 이벤트"))->setMessageText("최신 문화초대 이벤트 알려줘"));
-			$basicCard->addButton((new Button("KUNNECT 페이스북"))->setOsLink($link));
+			$basicCard->addButton((new Button("KUNNECT 페이스북 페이지"))->setOsLink($link));
 			$carousel->addCard($basicCard);
 
 			/** 1.3 학사일정 */
@@ -52,9 +52,9 @@
 			/** 1.4 건대신문 */
 			$basicCard = new BasicCard;
 			$basicCard->setThumbnail((new Thumbnail($temporary_thumbnail, $temporary_alt)));
-			$basicCard->addButton((new Button("최신기사 목록 보러가기"))->setMessageText("건대신문 최신기사 보여줘"));
+			$basicCard->addButton((new Button("건대신문 최신기사"))->setMessageText("건대신문 최신기사 보여줘"));
 			$basicCard->addButton((new Button("기사 제보(투고)하기"))->setWebLinkUrl("http://popkon.konkuk.ac.kr/com/jb.html"));
-			$basicCard->addButton((new Button("건대신문 페이스북"))->setWebLinkUrl("http://facebook.com/kkpressb"));
+			$basicCard->addButton((new Button("공식 페이스북 페이지"))->setWebLinkUrl("http://facebook.com/kkpressb"));
 			$carousel->addCard($basicCard);
 
 			$skillResponse->addResponseComponent($carousel);
