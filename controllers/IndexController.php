9c44d14f53fcb0047913ea74bc7b5a8b879bca7d
이동현 <dlehdanakf@konkuk.ac.kr>
2018-12-28 15:18:12 +0900

    KUNNECT => 쿠넥트

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -38,7 +38,7 @@
 			$basicCard->title = "건국대학교 학우에게만 제공되는 다양한 무료 이벤트!";
 			$basicCard->setThumbnail((new Thumbnail($temporary_thumbnail, $temporary_alt)));
 			$basicCard->addButton((new Button("최신 문화초대 이벤트"))->setMessageText("최신 문화초대 이벤트 알려줘"));
-			$basicCard->addButton((new Button("KUNNECT 페이스북 페이지"))->setOsLink($link));
+			$basicCard->addButton((new Button("쿠넥트 페이스북 페이지"))->setOsLink($link));
 			$carousel->addCard($basicCard);
 
 			/** 1.3 학사일정 */
