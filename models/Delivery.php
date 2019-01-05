5b5b2ba957607f293ad3d9ff0bdc965a5cc8712f
이동현 <dlehdanakf@konkuk.ac.kr>
2019-01-05 23:54:31 +0900

    delivery 모델에 label 추가

--- models/Delivery.php
+++ models/Delivery.php
@@ -1,14 +1,34 @@
 <?php
 	class Delivery extends BasicModel {
+		public $label;
 		public $title;
 		public $description;
 		protected $thumbnail_id;
 		public $contact;
 
+		public static function CREATE_BY_LABEL($label){
+			$query = B::DB()->prepare("SELECT * FROM delivery WHERE label = :l");
+			$query->execute([
+				':l' => $label
+			]);
+
+			$instance = new self;
+			if($query->rowCount() < 1)
+				throw new ModelNotFoundException(get_class($instance) . " 객체를 찾을 수 없습니다. label - " . $label);
+
+			$result = $query->fetch(PDO::FETCH_ASSOC);
+			foreach($result as $i => $v){
+				$instance->$i = $v;
+			}
+
+			return $instance;
+		}
+
 		public function save(){
 			$pdo = B::DB();
-			$query = $pdo->prepare("INSERT INTO delivery (title, description, thumbnail_id, contact) VALUE (:t, :d, :i, :c)");
+			$query = $pdo->prepare("INSERT INTO delivery (label, title, description, thumbnail_id, contact) VALUE (:l, :t, :d, :i, :c)");
 			$query->execute([
+				':l' => $this->label,
 				':t' => $this->title,
 				':d' => $this->description,
 				':i' => $this->thumbnail_id,
