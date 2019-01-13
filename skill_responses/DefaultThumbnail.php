2c78e73393a0f36839dc34b2f2065752e6d66ad7
이동현 <dlehdanakf@konkuk.ac.kr>
2019-01-14 01:57:55 +0900

    액박 카드 kung 서버 호스팅에서 자체 서버 주소로 전환

--- skill_responses/DefaultThumbnail.php
+++ skill_responses/DefaultThumbnail.php
@@ -1,7 +1,7 @@
 <?php
 	class DefaultThumbnail extends Thumbnail {
 		public function __construct(){
-			$url = "http://kung.kr/files/attach/images/247/123/041/006/bca9d3b106a8d89f73a9fc40daef22b2.png";
+			$url = B::GET_SERVICE_URL() . "/assets/images/default.png";
 			parent::__construct($url);
 		}
 	}
