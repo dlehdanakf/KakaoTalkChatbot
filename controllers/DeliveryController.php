04960a1f5eee795e0fed76f2b4559462ed5ebc90
이동현 <dlehdanakf@konkuk.ac.kr>
2019-01-20 00:23:46 +0900

    선 연장

--- controllers/DeliveryController.php
+++ controllers/DeliveryController.php
@@ -137,7 +137,7 @@
 					"👉 공유하기 버튼을 통해 친구에게 전달할 수 있습니다." . "\n" .
 					"👉 'MY메뉴 등록'을 통해 나만의 배달책자를 만들어보세요!"
 				;
-				$basicCard->description = $delivery->description ? "\n━━━━━━━━━━━━━━━━\n\n" . $delivery->description : " ";
+				$basicCard->description = $delivery->description ? "\n━━━━━━━━━━━━━━━━━━\n\n" . $delivery->description : " ";
 				$basicCard->addButton((new Button("배달 주문하기"))->setPhoneNumber($delivery->contact));
 				$skillResponse->addResponseComponent($basicCard);
 
